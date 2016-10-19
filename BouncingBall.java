994609b8c0daa763ab2f215d6dbcddb8d1346345
TamNguyen <phuongtam128@gmail.com>
2016-10-19 15:58:59 +0700

    resolve #2 - change mouse

--- BouncingBall.java
+++ BouncingBall.java
@@ -111,7 +111,7 @@ public class BouncingBall extends JFrame {
 			g.fillOval(10,10,40,40);
 			//Mouse trail
 			g.setColor(Color.RED);
-			g.fill3DRect(mouseX-10, mouseY-10, 20, 20,true);
+			g.fillOval(mouseX-10, mouseY-10, 20, 20);
 			
 		}
 	}
